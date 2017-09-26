1a998b5f0153b5e5f30b9f92626096cc472d112c
Claudio Acioli <kllaudyo@gmail.com>
2017-09-25 23:02:08 -0300

    Adicionando lib body-parser, e criando primeira rota post recebendo json

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,7 +1,15 @@
 
+var Log = require('../Log');
+
 module.exports = function(app){
-    app.get('/pagamentos',function(request, response){
-        console.log('recebendo requisicao para /pagamentos');
+
+    app.get('/pagamentos',function(request, response) {
+        Log.d('recebendo requisicao para /pagamentos');
         response.end('OK');
+    });
+
+    app.post('/pagamentos/pagamento',function(request, response){
+        Log.d(request.body);
+        response.send('ok\n');
     });
 };
